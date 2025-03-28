bd7a9cb9e3c3d92036b58d70280cfef51c098c47
Qexat <43090614+qexat@users.noreply.github.com>
2025-03-28 16:47:38 +0100

    add warning about running a program with compiler intrinsics enabled

--- lib/Hooks/Eval.ml
+++ lib/Hooks/Eval.ml
@@ -1,8 +1,19 @@
 module type CONFIG = Check.CONFIG
 
+let compiler_intrinsics_warning =
+  {|compiler intrinsics are not fully supported by the runtime and may cause unexpected crashes.|}
+;;
+
 let exec =
   fun ~nurse:_ ~painter (module Config : CONFIG) program ->
   let code = Runtime.Core.evaluate ~painter program in
+  let env =
+    if Config.use_compiler_intrinsics
+    then (
+      Printf.eprintf "WARNING: %s\n" compiler_intrinsics_warning;
+      Some Runtime.Core.intrinsics)
+    else None
+  in
   match code = 0 with
   | false -> None
   | true -> Some ()
