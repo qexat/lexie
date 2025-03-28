a343cea0d5439095cfd16e235d0b5e58b090bd26
Qexat <43090614+qexat@users.noreply.github.com>
2025-03-28 22:23:13 +0100

    batteries: add `List.map_on_cons`

--- lib/Batteries/List.ml
+++ lib/Batteries/List.ml
@@ -1,5 +1,12 @@
 include Stdlib.List
 
+let map_on_cons =
+  fun func list ->
+  match list with
+  | [] -> None
+  | _ -> Some (map func list)
+;;
+
 module Notation = struct
   let ( ++ ) = append
 end
--- lib/Batteries/List.mli
+++ lib/Batteries/List.mli
@@ -1,5 +1,7 @@
 include module type of Stdlib.List
 
+val map_on_cons : 'item1 'item2. ('item1 -> 'item2) -> 'item1 list -> 'item2 list option
+
 module Notation : sig
   val ( ++ ) : 'item. 'item t -> 'item t -> 'item t
 end
