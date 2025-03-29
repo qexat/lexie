7dc962f6d0c149a80c75e755066073fcafcc85df
Qexat <43090614+qexat@users.noreply.github.com>
2025-03-29 09:51:12 +0100

    make the compiler intrinsics warning use the clinic system

--- lib/Hooks/Eval.ml
+++ lib/Hooks/Eval.ml
@@ -1,13 +1,22 @@
-let compiler_intrinsics_warning =
-  {|compiler intrinsics are not fully supported by the runtime and may cause unexpected crashes.|}
+open Clinic
+
+let warn_about_compiler_intrinsics =
+  fun doctor painter ->
+  let review =
+    Doctor.emit_single_warning
+      painter
+      (doctor : Doctor.t).config
+      Diagnosis.Unsupported_intrinsics_at_runtime
+  in
+  Option.iter (Printf.eprintf "%s\n") review.details
 ;;
 
 let exec =
-  fun ~doctor:_ ~painter (config : Config.t) program ->
+  fun ~doctor ~painter (config : Config.t) program ->
   let env =
     if config.use_compiler_intrinsics
     then (
-      Printf.eprintf "WARNING: %s\n" compiler_intrinsics_warning;
+      warn_about_compiler_intrinsics doctor painter;
       Some Runtime.Core.intrinsics)
     else None
   in
