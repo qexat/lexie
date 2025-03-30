60e6f808537293cf5832c35550ec78c672e65813
Qexat <43090614+qexat@users.noreply.github.com>
2025-03-30 18:35:20 +0200

    hooks: eval: use config directly instead of the doctor's

--- lib/Hooks/Eval.ml
+++ lib/Hooks/Eval.ml
@@ -1,22 +1,19 @@
 open Clinic
 
 let warn_about_compiler_intrinsics =
-  fun doctor painter ->
+  fun painter config ->
   let review =
-    Doctor.emit_single_warning
-      painter
-      (doctor : Doctor.t).config
-      Diagnosis.Unsupported_intrinsics_at_runtime
+    Doctor.emit_single_warning painter config Diagnosis.Unsupported_intrinsics_at_runtime
   in
   Option.iter (Printf.eprintf "%s\n") review.details
 ;;
 
 let exec =
-  fun ~doctor ~painter (config : Config.t) program ->
+  fun ~doctor:_ ~painter (config : Config.t) program ->
   let env =
     if config.use_compiler_intrinsics
     then (
-      warn_about_compiler_intrinsics doctor painter;
+      warn_about_compiler_intrinsics painter config;
       Some Runtime.Core.intrinsics)
     else None
   in
