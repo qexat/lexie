724815879faa9eacd35fda0e734217015aea156d
Qexat <43090614+qexat@users.noreply.github.com>
2025-03-31 09:38:59 +0200

    add remaining documentation for the TAIL

--- lib/Compiler/TAIL/Sort.mli
+++ lib/Compiler/TAIL/Sort.mli
@@ -2,4 +2,6 @@ type t =
   | Prop
   | Type
 
+(** [show painter sort] produces a pretty-printable
+    representation of the [sort] using the [painter]. *)
 val show : (module Custom.Painter.TYPE) -> t -> string
--- lib/Compiler/TAIL/Statement.mli
+++ lib/Compiler/TAIL/Statement.mli
@@ -4,6 +4,14 @@ type t =
   | Let of Name.t * Kind.t option * Term.t
   | Print of Term.t
 
+(** [let' name ?annotation body] creates a [Let] statement given
+    a binding's [name], an optional [annotation] and its [body]
+    term. *)
 val let' : Name.t -> ?annotation:Kind.t -> Term.t -> t
+
+(** [print term] creates a [Print] statement given a [term]. *)
 val print : Term.t -> t
+
+(** [show painter statement] produces a pretty-printable
+    representation of the [statement] using the [painter]. *)
 val show : (module Painter.TYPE) -> t -> string
